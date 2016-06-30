6bbc3d05dc86c714b7be7365736f6c97581a7da8
Adam Quinton <quintonish@gmail.com>
2016-06-30 16:45:00 -0600

    Thumbnails
    OH YEAH!

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require ('express');
 var app = express();
 var fs = require('fs');
+var request = require('sync-request');
+var cheerio = require('cheerio');
+var http = require('http');
 app.set('view engine', 'ejs');
 var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,41 +24,71 @@ console.log(PORTNO+' is the magic port');
 // index page
 app.get('/', function(req, res) {
   //var arr = loadCsv('listing.csv');
-download('http://howtoterminal.com/listing.csv', 'listing.csv', function() {
-  var arr = [];
-  var filename = 'listing.csv'
-  fs.readFile(filename, 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    arr = data.split('\n')
-    arr.pop();
+  download('http://howtoterminal.com/listing.csv', 'listing.csv', function() {
+    var arr = [];
+    var filename = 'listing.csv'
+    fs.readFile(filename, 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      arr = data.split('\n')
+      arr.pop();
 
-    console.log(arr);
+      console.log(arr);
 
-    var list = [];
-    arr.forEach(function (line) {
-      var listItem = [];
-      var temp = line.split(',');
-      listItem.item = temp[0];
-      listItem.price = temp[1];
-      listItem.place = temp[2];
-      listItem.url = temp[3];
-      //TODO: add date
-      list.push(listItem);
+      var list = [];
+      arr.forEach(function (line) {
+        var listItem = [];
+        var temp = line.split(',');
+        listItem.item = temp[0];
+        listItem.price = temp[1];
+        listItem.place = temp[2];
+        listItem.url = temp[3];
+        //TODO: add date
+        list.push(listItem);
+      });
+
+  //todo: scrape for thumbs
+
+      scrapeThumbs(list).then(function(newList) {
+        //add prop to list
+        console.log('NEW LIST!!! ', newList);
+
+        console.log('RENDERING...');
+        res.render('index', {
+          itemType: process.env.ITEM_TYPE || 'Item',
+          listingData: list,
+          imgData: newList
+        });
+      });
     });
-
-    res.render('index', {
-      itemType: process.env.ITEM_TYPE || 'Item',
-      listingData: list
-    });
-
   });
 });
+//return a list of thumb urls
+var scrapeThumbs = function(urlArr) {
 
-});
+  var promise = new Promise(function(resolve, reject) {
+    var list = [];
+    urlArr.forEach(url => {
+      console.log('MAKING REQUEST ', url.url);
 
-var http = require('http');
+      var res = request('GET', url.url);
+      console.log('DONE...');
+
+      var $ = cheerio.load(res.getBody());
+      //var thumbnail = $('.swipe-wrap').find('div').children('img')[0].src;
+      var thumbnail = $('.swipe-wrap').find('div').children('img')[0].attribs.src;
+
+      console.log('thumbnail ',thumbnail);
+      list.push(thumbnail);
+    });
+    if (true) {
+      resolve(list);
+    } else
+        reject(Error("Broked shoot dangit"))
+    });
+    return promise;
+};
 
 var download = function(url, dest, cb) {
   var file = fs.createWriteStream(dest);
