d190f5676d7dd9a74313bf2c215263f07e259af4
Adam <8365885+zvakanaka@users.noreply.github.com>
2016-08-02 18:57:40 -0600

    MongoDB checks if new listing

--- app.js
+++ app.js
@@ -10,8 +10,52 @@ app.set('view engine', 'ejs');
 var path = require('path');
 var middleware = require('./lib/middleware/middleware.js');
 var sendMail = require('./lib/js/sendMail.js');
+var mongoose = require ("mongoose");
 
 env(__dirname+'/.env');
+
+var MONGO_URI = process.env.MONGO_URI || '';
+mongoose.connect(MONGO_URI, function (err, res) {
+  if (err) {
+    console.log ('ERROR connecting to: ' + MONGO_URI + '. ' + err);
+  } else {
+    console.log ('Yay connected to: ' + MONGO_URI);
+  }
+});
+//Item schema holds items and itemTypes
+var itemSchema = new mongoose.Schema({
+    //seq: { type: Number, unique:true, sparse:true },
+    itemType: { type: String, trim: true },
+    link: { type: String, trim: true, unique:true, sparse:true },
+    title: { type: String, trim: true },
+    price: { type: Number},
+    info: { type: String, trim: true },
+    place: { type: String, trim: true },
+    date: { type: Date},
+    creationDate: Date
+});
+var ItemModel = mongoose.model('Items', itemSchema);
+function saveItem(item) {
+  var status = { err: null };
+  item.save(function (err) {
+    if (err) status.err = err;
+  });
+  return status;
+}
+function insertItem(item) {
+  var itemToInsert = new ItemModel ({
+    itemType: item.itemType,
+    link: item.link,
+    title: item.title,
+    price: parseFloat(item.price.replace(',','')),
+    info: item.info,
+    place: item.place,
+    date: item.date,
+    creationDate: new Date()
+  });
+  var insertStatus = saveItem(itemToInsert);
+  return insertStatus;
+}
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/components', express.static(__dirname + '/components'));
@@ -88,10 +132,10 @@ var scrapeKsl = function (searchTerm, options) {
           img = 'images/not-found.png';
         }
         var title = $(this).find('.adTitle').text().trim()
-        var itemUrl = siteUrl + $(this).find('.listlink')['0']['attribs']['href'];
+        var link = siteUrl + $(this).find('.listlink')['0']['attribs']['href'];
         var price = $(this).find('.priceBox').text().trim();
         price = price.substring(1, price.length-2);
-        listings.push({img: img, title: title, itemUrl: itemUrl, price: price});
+        listings.push({img: img, title: title, link: link, price: price});
       });
       resolve(listings);
     } else {
@@ -131,31 +175,69 @@ var scrapeKslCars = function (searchTerm, options) {
 
     if ($('.listing').length != 0) {
       $(".listing").each(function(index) {
-        console.log("**********************");
         var img = $(this).find('.photo')['0']['attribs']['style'];
         if (img !== undefined) {
-          img = img.substring(img.indexOf("https://"), img.indexOf(')'));
+          img = img.substring(img.indexOf("url(")+4, img.indexOf(')'));
         } else {
           img = 'images/not-found.png';
         }
-        console.log(img);
         var title = $(this).find('.title').text().trim();
-        console.log(title);
-        var itemUrl = 'http://www.ksl.com' + $(this).find('.title .link')['0']['attribs']['href'];
-        console.log(itemUrl);
+        var link = 'http://www.ksl.com' + $(this).find('.title .link')['0']['attribs']['href'];
         var price = $(this).find('.price').text().trim().substr(1);
-        console.log(price);
-        //TODO: add miles
-        var info = '';
+        var mileage = $(this).find('.mileage').text().trim();
+        var dateTemp = $(this).find('.nowrap').text().trim();
+        var dateFormatted = dateTemp.substring(dateTemp.indexOf('min(')+4, dateTemp.indexOf(')'));
+        var dateVal ="/Date("+dateFormatted+"000)/";
+        var date = new Date(parseFloat(dateVal.substr(6)));
+        //TODO: get real place
+        var place = 'UT';
         console.log("**********************");
-        listings.push({img: img, title: title, itemUrl: itemUrl, price: price, info: info});
+
+        var item = {itemType: 'Car',
+            img: img,
+            title: title, link: link,
+            price: price, info: mileage,
+            place: place, date: date};
+          console.log(item);
+        //TODO: insert in mongodb
+        var query = ItemModel.find({ link: link });
+        query.exec(function(err, result) {
+          if (!err) {
+              if (result.length == 0) {
+                insertItem(item);
+                sendMail.sendText([ item ]);
+              }
+          } else {
+            console.log(err);
+          }
+        });
+        listings.push(item);
       });
-      sendMail.sendText(listings);
       resolve(listings);
     } else {
-      console.log('ERROR: no listings');
       reject(Error('Error: no listings'));
     }
   });
   return promise;
 }
+
+app.get('/db/all', function(req, res) {
+  var query = ItemModel.find();
+  query.exec(function(err, result) {
+    if (!err) {
+        res.send(result);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.get('/db/reset', function(req, res) {
+  ItemModel.remove({}, function(err) {
+    if (err) {
+      res.send('ERROR: resetting all entries failed');
+    } else {
+      res.send('Success: Reset all entries');
+    }
+  });
+});
