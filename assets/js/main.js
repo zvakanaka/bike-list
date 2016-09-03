439083c5726203d51fd19f687062235af5659e1e
Adam <8365885+zvakanaka@users.noreply.github.com>
2016-09-02 19:07:41 -0700

    Add so much junk so users can store their own scrapes
    Not working. Still need to implement the scrape endpoint fully.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /***************************
  * DATABASE
  ***************************/
-function database(stringified, url) {
+function database(url, data) {
   var http = new XMLHttpRequest();
   http.open("POST", url, true);
   http.setRequestHeader("Content-type", "application/json; charset=utf-8");
@@ -13,5 +13,41 @@ function database(stringified, url) {
       data = JSON.parse(data);
     }
   };
-  http.send(stringified);
+  http.send(JSON.stringify(data));
 }
+
+
+function addScrape(options) {
+
+  var searchTerm = options.searchTerm;
+  var maxPrice = options.maxPrice;
+  var insert = options.insert;
+  var sendMessage = options.sendMessage;
+  var sendTo = options.sendTo;
+  var section = options.section;
+  var maxMiles = options.maxMiles;
+  var name = options.scrapeName;
+  var site = options.site;
+
+  //TODO: change cl to something else
+  database('/new/cl', options);
+}
+
+var btnSubmit = document.getElementById('btn-add-scrape');
+btnSubmit.addEventListener('click', function() {
+  //TODO: validate form before parsing
+  console.log('Button pressed');
+  var scrapeOptions = {
+    searchTerm: document.getElementById('input-search-term').value,
+    maxPrice: document.getElementById('input-max-price').value,
+    insert:  document.getElementById('input-insert') || false,
+    sendMessage:  document.getElementById('checkbox-send-message').value,
+    sendTo:  document.getElementById('input-send-to').value,
+    section:  document.getElementById('select-section').value,
+    maxMiles:  document.getElementById('input-max-miles').value,
+    scrapeName:  document.getElementById('input-name').value,
+    site:  document.getElementById('select-site').value
+  };
+
+  addScrape(scrapeOptions);
+});
