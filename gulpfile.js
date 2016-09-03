3ead975986eed8033cfbb4f87a0fd2fdee462ffa
zvakanaka <8365885+zvakanaka@users.noreply.github.com>
2016-09-02 19:07:41 -0700

    Add so much junk so users can store their own scrapes
    Not working. Still need to implement the scrape endpoint fully.

--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,7 @@ gulp.task('browser-sync', ['nodemon'], function() {
     // for more browser-sync config options: http://www.browsersync.io/docs/options/
     browserSync({
       proxy: 'http://localhost:' + PORTNO,
-      browser: 'google-chrome'
+      browser: process.env.BROWSER || 'google-chrome'
     });
   } else {
     console.log('ERROR: Must set NODE_ENV=dev in .env for browserSync');
