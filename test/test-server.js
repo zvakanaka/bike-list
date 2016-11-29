741153ac6db4854caeaecda1a92b326250522f70
Adam Quinton <quintonish@gmail.com>
2016-11-29 12:29:42 -0700

    add more tests

--- test/test-server.js
+++ test/test-server.js
@@ -5,18 +5,34 @@ var should = chai.should();
 
 chai.use(chaiHttp);
 
+var mongoService = require('../lib/js/mongoService.js');
+var scrapers = require('../lib/js/scrapers.js');
 
-describe('Blobs', function() {
-  it('should list ALL blobs on /blobs GET', function(done) {
-  chai.request(server)
-    .get('/blobs')
-    .end(function(err, res){
-      res.should.have.status(200);
-      done();
-    });
+
+describe('Delete scrapes with no user ID', function() {
+  it('should return an error', function() {
+    var err = new Error('Failed to specify UserId');
+    mongoService.deleteScrapes().should.equal(err);
+  });
 });
-  it('should list a SINGLE blob on /blob/<id> GET');
+
+describe('Get all active scrapes ', function() {
+  it('should return multiple scrapes', function() {
+    var status = mongoService.deleteScrapes();
+    status.should.Throw;
+  });
+});
+
+describe('Get city name from zip ', function() {
+  it('should return corresponding city name', function() {
+    var city = scrapers.getCity(90620);
+    city.should.equal('orangecounty');
+    city = scrapers.getCity(83440);
+    city.should.equal('eastidaho');
+  });
+});
+
+
   it('should add a SINGLE blob on /blobs POST');
   it('should update a SINGLE blob on /blob/<id> PUT');
   it('should delete a SINGLE blob on /blob/<id> DELETE');
-});
