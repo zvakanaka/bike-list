80c87783c982a359486f971a6a9c0960b45db16e
zvakanaka <quintonish@gmail.com>
2017-02-08 22:55:29 -0700

    add aaa not working, but not breaking anything old

---
+++ test/test-scraper-cars-aaa.js
@@ -0,0 +1,33 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../app');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+var scrapers = require('../lib/js/scrapers.js');
+
+describe('Scrape aaa cars for Fords ', function() {
+  this.timeout(10000);
+  it('should return at least one car', function(done) {
+    var search = {
+      searchTerm: 'ford/explorer',
+      maxPrice: 11000,
+      insert: true, // does not carry through to mongodb
+      sendMessage: false,
+      sendTo: 'nobody@ihopethisdoesntexist.com',
+      userId: 123,
+      section: '',
+      maxMiles: 120000,
+      scrapeName: 'Raharizuandrinarina',
+      site: 'aaacars',
+      zip: 90620
+    };
+    scrapers.scrape(search)
+      .then(function(res) {
+        console.log(res.listings.length + ' items found');
+        res.listings.length.should.be.at.least(1);
+        done();
+      });
+  });
+});
